73230d04e0a34d0b1c14544e3aea0b2907648fd7
BCT <benjamin.coignet@gmail.com>
2014-06-19 11:46:46 +0200

    Ajout grade texte menu

--- EMS/global/menu.php
+++ EMS/global/menu.php
@@ -9,27 +9,32 @@
 		 } else {
 			echo '<h3>Espace membre</h3>
 			<p>Bienvenue, ' . htmlspecialchars($_SESSION['utilisateur']->getNomUtilisateur()) . '.</p>
-			<ul>';
+			';
 
 				switch($_SESSION['utilisateur']->getGrade()) {
 					case '1':
-					echo '<li><a href="' . CHEMIN_BASE . 'index.php?module=membres&action=listing">Liste des membres </a></li>';
-					echo '<li><a href="' . CHEMIN_BASE . 'index.php?module=courses&action=listing">Liste des courses </a></li>';
-					echo '<li><a href="' . CHEMIN_BASE . 'index.php?module=courses&action=creer">Créer une course </a></li>';
-					echo '<li><a href="' . CHEMIN_BASE . 'index.php?module=utilisateurs&action=listing">Liste des utilisateurs </a></li>';
-					echo '<li></li>';
+						echo '<ul>Admin';
+						echo '<li><a href="' . CHEMIN_BASE . 'index.php?module=membres&action=listing">Liste des membres </a></li>';
+						echo '<li><a href="' . CHEMIN_BASE . 'index.php?module=courses&action=listing">Liste des courses </a></li>';
+						echo '<li><a href="' . CHEMIN_BASE . 'index.php?module=courses&action=creer">Créer une course </a></li>';
+						echo '<li><a href="' . CHEMIN_BASE . 'index.php?module=utilisateurs&action=listing">Liste des utilisateurs </a></li>';
+						echo '<li></li>';
+						echo '</ul>';
 
 					case '10':
-					echo '<li><a href="' . CHEMIN_BASE . 'index.php/"> rien 10 </a></li>';
-					echo '<li></li>';
+						echo '<ul>Bureau';
+						echo '<li><a href="' . CHEMIN_BASE . 'index.php/"> rien 10 </a></li>';
+						echo '<li></li>';
+						echo '</ul>';
 
 					case '100':
-					echo '<li><a href="' . CHEMIN_BASE . 'index.php/"> rien 100</a></li>';
+						echo '<ul>Membre';
+						echo '<li><a href="' . CHEMIN_BASE . 'index.php/"> rien 100</a></li>';
+						echo '</ul>';
 					default:
-					echo '<li><a href="' . CHEMIN_BASE . 'index.php?module=utilisateurs&action=deconnexion">Déconnexion</a></li>';
+					echo '<a href="' . CHEMIN_BASE . 'index.php?module=utilisateurs&action=deconnexion">Déconnexion</a>';
 				}
 
-			echo '</ul>';
 		 }
 		 ?>
 </div>
